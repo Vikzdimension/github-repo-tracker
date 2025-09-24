4d60fed52c50526d1591d8aca3648266fba535f5
Vikzdimension <vivek.lode1@gmail.com>
2025-09-24 20:13:18 +0530

    fetching and saving repo details to DB and now using get() method instead of raw data

--- backend/projects/api_integration.py
+++ backend/projects/api_integration.py
@@ -2,13 +2,37 @@ import os
 import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Project
 
 GITHUB_API_URL = "https://api.github.com/repos"
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
 @api_view(['GET'])
-def fetch_github_trending(request, owner, repo):
+def fetch_github_repo(request, owner, repo):
+
+    url = f"{GITHUB_API_URL}/{owner}/{repo}"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"token {GITHUB_TOKEN}" if GITHUB_TOKEN else ""
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return Response({"error": "Could not fetch repo from GitHub"},
+                        status=response.status_code)
+
+    data = response.json()
+    return Response({
+        "name": data.get("name"),
+        "description": data.get("description"),
+        "language": data.get("language"),
+        "stars": data.get("stargazers_count", 0),
+    })
+
+@api_view(['POST'])
+def save_github_repo(request, owner, repo):
     url = f"{GITHUB_API_URL}/{owner}/{repo}"
     headers = {
         "Accept" : "application/vnd.github+json",
@@ -17,8 +41,30 @@ def fetch_github_trending(request, owner, repo):
 
     response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        return Response(response.json())
-    else:
-        return Response({"error": f"Could not fetch repo"}, 
+    if response.status_code != 200:
+        return Response({"error": f"Could not fetch repo from GitHub"},
                         status=response.status_code)
+    
+    data = response.json()
+
+    project, created = Project.objects.update_or_create(
+        name=data['name'],
+        defaults={
+            'description': data.get('description', ''),
+            'language': data.get('language', ''),
+            'stars': data.get('stargazers_count', 0),
+        }
+    )
+    
+    return Response({
+        "message": "Repo Saved Sucessfully" if created else "Repo updated sucessfully",
+        "project": {
+            "id": project.id,
+            "name": project.name,
+            "description": project.description,
+            "language": project.language,
+            "stars": project.stars,
+            "created_at": project.created_at
+        }
+    })
+
--- backend/projects/urls.py
+++ backend/projects/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProjectViewSet
-from .api_integration import fetch_github_trending
+from .api_integration import fetch_github_repo, save_github_repo
 
 router = DefaultRouter()
 router.register(r'', ProjectViewSet, basename='project')
 
 urlpatterns = [
     path('projects/', include(router.urls)),
-    path('github/<str:owner>/<str:repo>/', fetch_github_trending, name='github-repo'),
+    path('github/<str:owner>/<str:repo>/', fetch_github_repo, name='github-repo'),
+    path('github/save/<str:owner>/<str:repo>/', save_github_repo, name='save-github-repo')
 ]
 
 
