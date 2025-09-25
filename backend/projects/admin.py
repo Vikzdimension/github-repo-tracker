efd39f71313b4730e3d741ac10570ad10fb1a2fa
Vikzdimension <vivek.lode1@gmail.com>
2025-09-25 10:08:09 +0530

    assets file name now fixed without hashes

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django.conf import settings
 from .models import Project
-import os
-import glob
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):    
@@ -17,21 +14,6 @@ class ProjectAdmin(admin.ModelAdmin):
     def changelist_view(self, request, extra_context=None):
         extra_context = extra_context or {}
         extra_context['title'] = 'GitHub Repository Dashboard'
-        
-        # Find asset files dynamically
-        static_root = settings.STATIC_ROOT or os.path.join(settings.BASE_DIR, 'staticfiles')
-        assets_path = os.path.join(static_root, 'assets')
-        
-        css_files = []
-        js_files = []
-        
-        if os.path.exists(assets_path):
-            css_files = [f'assets/{os.path.basename(f)}' for f in glob.glob(os.path.join(assets_path, 'index-*.css'))]
-            js_files = [f'assets/{os.path.basename(f)}' for f in glob.glob(os.path.join(assets_path, 'index-*.js'))]
-        
-        extra_context['css_files'] = css_files
-        extra_context['js_files'] = js_files
-        
         return super().changelist_view(request, extra_context)
     
     def get_queryset(self, request):
