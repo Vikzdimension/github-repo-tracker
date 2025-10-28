611c5c76aeb0157919a126029b136528d5db4883
Vikzdimension <vivek.lode1@gmail.com>
2025-10-28 21:10:05 +0530

    chore: favicon added and resolved

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -194,6 +194,6 @@ if DEBUG:
 # REACT 
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'frontend' / 'admin-dashboard' / 'dist' / 'assets',
+    BASE_DIR / 'static',
     BASE_DIR / 'frontend' / 'admin-dashboard' / 'dist',
 ]
